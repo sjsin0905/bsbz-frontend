d2f56637a82fe1907f6b4af2b8b90fc273161c3b
조용훈 <sjsin0905@naver.com>
2023-09-05 17:38:29 +0900

    ADD: loginPage - create loginForm

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -17,17 +17,60 @@ const Container = styled.div`
 const Main = styled.div`
   height: 68vh;
   width: 100vw;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: end;
+  align-items: center;
+`;
+
+const LoginForm = styled.div`
+  height: 50vh;
+  border: 1px solid;
+  width: 50vw;
+  padding-top: 48px;
+  padding-left: 48px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: start;
+
+  form {
+    text-align: center;
+    padding-left: 48px;
+  }
+
+  input {
+    border: 0px;
+    border-radius: 12px;
+    text-align: center;
+    width: 280px;
+    height: 30px;
+    margin: 10px;
+  }
 `;
 
 export default function Login() {
   const [isLoading, setIsLoading] = useState(false);
-
+  const deleteValue = () => {};
   return (
     <>
       {isLoading && <Loading />}
       <Navigation />
       <Container>
-        <Main> </Main>
+        <Main>
+          <LoginForm>
+            <form action="#" method="post">
+              <h2>투자의 새로운 패러다임</h2>
+              <br></br>
+              <br></br>
+              <input type="text" value={"ID"} onClick={deleteValue}></input>
+              <br></br>
+              <input type="text" value={"PW"}></input>
+            </form>
+          </LoginForm>
+        </Main>
       </Container>
       <Footer />
     </>
