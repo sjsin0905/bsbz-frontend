20274974e28f7e2b9fcf8be84780c51d17971815
Lee-Junmo <108059303+ezurno@users.noreply.github.com>
2023-09-11 10:05:43 +0900

    MOD: create member-list

---
+++ src/jsons/memberList.js
@@ -0,0 +1,32 @@
+export const memberList = [
+  {
+    imgUrl: "https://avatars.githubusercontent.com/u/97649344?v=4",
+    memberName: "정승윤",
+    gitHubUrl: "https://github.com/syoon6682",
+  },
+  {
+    imgUrl: "https://avatars.githubusercontent.com/u/108059303?v=4",
+    memberName: "이준모",
+    gitHubUrl: "https://github.com/ezurno",
+  },
+  {
+    imgUrl: "https://avatars.githubusercontent.com/u/97722177?v=4",
+    memberName: "조용훈",
+    gitHubUrl: "https://github.com/sjsin0905",
+  },
+  {
+    imgUrl: "https://avatars.githubusercontent.com/u/90839206?v=4",
+    memberName: "이주원",
+    gitHubUrl: "https://github.com/2oo1s",
+  },
+  {
+    imgUrl: "https://avatars.githubusercontent.com/u/139327486?v=4",
+    memberName: "정다현",
+    gitHubUrl: "https://github.com/dahyeon730",
+  },
+  {
+    imgUrl: "https://avatars.githubusercontent.com/u/81132057?v=4",
+    memberName: "이채영",
+    gitHubUrl: "https://github.com/lcy923",
+  },
+];
